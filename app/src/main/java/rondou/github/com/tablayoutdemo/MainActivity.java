2a4dd2f83b3edc50f4c7671037dbd3088f1d5d09
Rondou Chen <40and44sis@gmail.com>
2015-09-18 18:09:01 +0800

    Invoke SmartTabLayout

--- app/src/main/java/rondou/github/com/tablayoutdemo/MainActivity.java
+++ app/src/main/java/rondou/github/com/tablayoutdemo/MainActivity.java
@@ -1,19 +1,36 @@
 package rondou.github.com.tablayoutdemo;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
+
+    PagerAdapter pagerAdapter;
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        SmartTabLayout tab = (SmartTabLayout) findViewById(R.id.tab);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
 
+        pagerAdapter = new SampleFragmentPagerAdapter(getSupportFragmentManager(), getApplicationContext());
+        pager.setAdapter(pagerAdapter);
+        tab.setViewPager(pager);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +53,32 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 3;
+        private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+        private Context context;
+
+        public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // Generate title based on item position
+            return tabTitles[position];
+        }
+    }
+
 }
